cd76fb5ad1b599840d18ab2fa78c537ffafdf82a
vladimir.silva.ortiz <vladimir.silva.ortiz@6ca97c9e-103c-0410-a6d1-f1f325dd6ca3>
2007-10-17 16:47:49 +0000

    World Wind Contributed layers. Initial import.

---
+++ trunk/WWContrib-025/src/worldwind/contrib/layers/TiledWMSLayer.java
@@ -0,0 +1,173 @@
+package worldwind.contrib.layers;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.avlist.AVListImpl;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.layers.BasicTiledImageLayer;
+import gov.nasa.worldwind.util.LevelSet;
+import gov.nasa.worldwind.util.Tile;
+import gov.nasa.worldwind.util.TileUrlBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Layer representing a WMS 1.1.1 Capabilities<br/>
+ * From the NASA WMS server at: <br/>
+ * 	<pre>http://worldwind21.arc.nasa.gov/geoserver/wms?service=WMS&request=GetCapabilities</pre><br/>
+ * <b>This layer represents US Counties: </b> <pre>
+ *          String url		= "http://worldwind21.arc.nasa.gov/geoserver/wms";
+ *        	String layer	= "topp:us_counties"; 
+ *        	String styles	= "borders_black"; 
+ *        	String cacheName	= "Earth/USCountyBoundaries";
+ *        	
+ *       	TiledWMSLayer.LayerDescriptor service = new TiledWMSLayer.LayerDescriptor(url
+ *        				, layer		// layer name
+ *        				, styles	// styles
+ *        				, TiledWMSLayer.LayerDescriptor.FMT_PNG 
+ *        				, 13 		// levels
+ *        				, 512, 512	// w h
+ *        				, cacheName// WW cache name for this layer
+ *        				);
+ *       	TiledWMSLayer l = new TiledWMSLayer(service);</pre>
+ * For political boundaries use:<br/> <pre>
+ *	layer = "topp:cia"
+ *	cache name = "Earth/PoliticalBoundaries"; 
+ * </pre>
+ * @author Vladimir Silva
+ */
+public class TiledWMSLayer extends BasicTiledImageLayer //extends TiledImageLayer
+{
+	private static final Logger logger = Logger.getLogger(TiledWMSLayer.class);
+			
+	private final static String KEY_STYLES = "wms.layer.styles";
+	private final static String KEY_FORMAT = "wms.layer.fmt";
+	private final static String KEY_SRS = "wms.layer.srs";
+	private final static String KEY_TIME = "wms.layer.time";
+	private final static String KEY_VERSION = "wms.layer.version";
+
+	public final static String VERSION_1_1_1 = "1.1.1";
+	public final static String VERSION_1_3_0 = "1.3.0";
+
+	private TiledWMSLayerDescriptor descriptor;
+
+	static public final String FMT_PNG = "image/png";
+	static public final String FMT_GIF = "image/gif";
+	static public final String FMT_JPG = "image/jpg";
+	
+	public TiledWMSLayer (TiledWMSLayerDescriptor descriptor)
+	{
+        super(makeLevels(descriptor, new WMSURLBuilder()));
+        setDescriptor(descriptor);
+        setUseTransparentTextures(true);
+		logger.debug(this.descriptor);
+	}
+
+
+    private static LevelSet makeLevels(TiledWMSLayerDescriptor service, WMSURLBuilder urlBuilder )
+    {
+        AVList params = new AVListImpl();
+        
+		logger.debug("WMS Descriptor=" + service);
+
+		params.setValue(AVKey.DISPLAY_NAME, service.name);
+        params.setValue(AVKey.TILE_WIDTH, service.getWidth());
+        params.setValue(AVKey.TILE_HEIGHT, service.getHeight());
+        params.setValue(AVKey.DATA_CACHE_NAME, service.getCacheName());
+        params.setValue(AVKey.SERVICE, service.getServiceUrl());
+        params.setValue(AVKey.DATASET_NAME, service.getLayerName());
+        params.setValue(AVKey.FORMAT_SUFFIX, service.getFormat().replaceAll("image/", "."));
+        params.setValue(AVKey.NUM_LEVELS, service.getLevels());
+        params.setValue(AVKey.NUM_EMPTY_LEVELS, 0);
+        params.setValue(AVKey.LEVEL_ZERO_TILE_DELTA, new LatLon(Angle.fromDegrees(36d), Angle.fromDegrees(36d)));
+        params.setValue(AVKey.SECTOR, service.getBBox());
+        params.setValue(AVKey.TILE_URL_BUILDER, urlBuilder);
+        params.setValue(KEY_STYLES, service.getStyle().name);
+        params.setValue(KEY_FORMAT, service.getFormat());
+        params.setValue(KEY_SRS, service.getSrs());
+        params.setValue(KEY_TIME, service.getTime());
+        params.setValue(KEY_VERSION, service.getVersion());
+        
+        return new LevelSet(params);
+    }
+
+    private static class WMSURLBuilder implements TileUrlBuilder
+    {
+        public URL getURL(Tile tile) throws MalformedURLException
+        {
+        	AVList params =  tile.getLevel().getParams();
+        	
+            StringBuffer sb = new StringBuffer(tile.getLevel().getService());
+            if (sb.lastIndexOf("?") != sb.length() - 1)
+                sb.append("?");
+            sb.append("service=WMS&request=GetMap");
+            sb.append("&layers=");
+            sb.append(tile.getLevel().getDataset());
+            sb.append("&srs=" + params.getStringValue(KEY_SRS)); 
+            sb.append("&width=");
+            sb.append(tile.getLevel().getTileWidth());
+            sb.append("&height=");
+            sb.append(tile.getLevel().getTileHeight());
+
+        	//Sector bbox = (Sector)params.getValue(AVKey.SECTOR);
+            Sector s = tile.getSector();
+
+            final String time = params.getStringValue(KEY_TIME);
+             
+            if ( time != null ) {
+            	sb.append("&time=" + time);
+            }
+
+        	// static layer: use tile sector
+            sb.append("&bbox=");
+            sb.append(s.getMinLongitude().getDegrees());
+            sb.append(",");
+            sb.append(s.getMinLatitude().getDegrees());
+            sb.append(",");
+            sb.append(s.getMaxLongitude().getDegrees());
+            sb.append(",");
+            sb.append(s.getMaxLatitude().getDegrees());
+            
+            final String styles = params.getStringValue(KEY_STYLES);
+            if ( styles != null ) sb.append("&styles=" + styles);
+
+            final String format = params.getStringValue(KEY_FORMAT);
+            if ( format != null ) sb.append("&format=" + format);
+
+            final String version = params.getStringValue(KEY_VERSION);
+            if ( version != null ) sb.append("&version=" + version);
+            
+            sb.append("&transparent=true");
+            
+            logger.debug("TiledWMSLayer: " + tile.getPath() + " u=" + sb);            
+            return new java.net.URL(sb.toString());
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+    	return (descriptor != null ? descriptor.getTitle() : null ) ;
+    }
+
+
+	/**
+	 * @param descriptor the descriptor to set
+	 */
+	public void setDescriptor(TiledWMSLayerDescriptor descriptor) {
+		this.descriptor = descriptor;
+	}
+
+
+	/**
+	 * @return the descriptor
+	 */
+	public TiledWMSLayerDescriptor getDescriptor() {
+		return descriptor;
+	}
+	
+}
---
+++ trunk/WWContrib-025/src/worldwind/contrib/todo/layers/VirtualEarthLayer.java
@@ -0,0 +1,243 @@
+/**
+ * 
+ */
+package worldwind.contrib.todo.layers;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import worldwind.contrib.Messages;
+
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.avlist.AVListImpl;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.layers.BasicTiledImageLayer;
+import gov.nasa.worldwind.util.LevelSet;
+import gov.nasa.worldwind.util.Tile;
+import gov.nasa.worldwind.util.TileUrlBuilder;
+import gov.nasa.worldwind.view.BasicOrbitView;
+
+/**
+ * @author Owner
+ *
+ */
+public class VirtualEarthLayer extends BasicTiledImageLayer  
+{
+	/**
+	 * Constructor 
+	 */
+	public VirtualEarthLayer(WorldWindowGLCanvas world) {
+        super(makeLevels(new URLBuilder(world))); //builder));
+        this.setUseTransparentTextures(true);
+	}
+
+    private static LevelSet makeLevels(URLBuilder urlBuilder)
+    {
+        AVList params = new AVListImpl();
+
+        params.setValue(AVKey.TILE_WIDTH, 256);
+        params.setValue(AVKey.TILE_HEIGHT, 256);
+        params.setValue(AVKey.DATA_CACHE_NAME, "Earth/MS Virtual Earth");
+        
+        // dummy values
+        params.setValue(AVKey.SERVICE, "http://dummy.com/");
+        params.setValue(AVKey.DATASET_NAME, "dummy");
+        params.setValue(AVKey.FORMAT_SUFFIX, ".png");
+        
+        params.setValue(AVKey.NUM_LEVELS, 19);
+        params.setValue(AVKey.NUM_EMPTY_LEVELS, 0);
+        params.setValue(AVKey.LEVEL_ZERO_TILE_DELTA, new LatLon(Angle.fromDegrees(36d), Angle.fromDegrees(36d)));
+        params.setValue(AVKey.SECTOR, Sector.FULL_SPHERE);
+        params.setValue(AVKey.TILE_URL_BUILDER, urlBuilder);
+
+        return new LevelSet(params);
+    }
+	
+    /**
+     * 
+     * @author Owner
+     *
+     */
+    private static class URLBuilder implements TileUrlBuilder
+    {
+    	private String mapType = "h";
+    	private String mapExtension = ".jpeg";
+
+    	private WorldWindowGLCanvas world;
+    	
+		private final double earthRadius = 6378137;
+		private final double earthCircum = earthRadius * 2.0 * Math.PI;
+		private final double earthHalfCirc = earthCircum / 2;
+    	
+    	public URLBuilder(WorldWindowGLCanvas world){ 
+            this.world = world;
+    	}
+    	
+    	public URL getURL(Tile tile) throws MalformedURLException
+        {
+    		Sector s 		= tile.getSector();
+    		LatLon latLon 	= s.getCentroid();
+    		
+    		BasicOrbitView view = (BasicOrbitView)world.getView();
+    		//Globe globe 		= world.getModel().getGlobe();
+    		
+    		Position eyePoint = view.getEyePosition();// globe.computePositionFromPoint(view.getEyePoint());
+    		
+    		double altitude 	= eyePoint.getElevation();
+    		double factor 		= altitude / earthRadius;
+    		double distance 	= view.getAltitude();
+    		
+    		Angle trueViewRange; // = view.getFieldOfView().degrees;
+    		
+    		if ( factor < 1 )
+    			trueViewRange = Angle.fromRadians(Math.abs(Math.asin((distance) / earthRadius))*2);
+    		else
+    			trueViewRange = Angle.fromRadians(Math.PI);
+    				
+    		
+    		int zoomLevel =  GetZoomLevelByTrueViewRange(trueViewRange.degrees);
+    		
+    		double lat = eyePoint.getLatitude().degrees; // latLon.getLatitude().degrees;
+    		double lon = eyePoint.getLongitude().degrees; // latLon.getLongitude().degrees;
+//    		double alt = eyePoint.getElevation();
+    		
+			int meterY = LatitudeToYAtZoom(lat, zoomLevel);
+			int meterX = LongitudeToXAtZoom(lon, zoomLevel);
+
+			final int imageSize = 256; //constant
+			int col = meterX / imageSize;
+			int row = meterY / imageSize;
+
+System.out.println("altitude=" + altitude 
+		+ " trueViewRange=" + trueViewRange + " Sector:" + s 
+		+ " zoom lev=" + zoomLevel	+ " lat=" + lat 
+		+ " lon=" + lon + " mY=" + meterY + " mX=" + meterX
+		+ " col=" + col + " row=" + row);
+
+			String quadKey = TileToQuadKey(col, row, zoomLevel);
+			
+    		if ( mapType.equals("h") || mapType.equals("a")) 
+    			mapExtension = ".jpeg";
+    		else if ( mapType.equals("r"))
+    			mapExtension = ".png";
+
+
+            String sb = "http://" + getMapType()
+            	+ quadKey.charAt(quadKey.length() - 1)
+            	+ ".ortho.tiles.virtualearth.net/tiles/"
+            	+ mapType + quadKey + mapExtension 
+            	+ "?g=1";
+
+//System.out.println("quad key=" + quadKey + " m type=" + mapType + " ext=" + mapExtension + " url=" + sb);
+            return new java.net.URL(sb);
+        }
+
+		private static String TileToQuadKey(int tx, int ty, int zl)
+		{
+			String quad = "";
+			for (int i = zl; i > 0; i--)
+			{
+				int mask = 1 << (i - 1);
+				int cell = 0;
+				if ((tx & mask) != 0)
+				{
+					cell++;
+				}
+				if ((ty & mask) != 0)
+				{
+					cell += 2;
+				}
+				quad += cell;
+			}
+			return quad;
+		}
+    	
+		private static double DegToRad(double d)
+		{
+			return d * Math.PI / 180.0;
+		}
+    	
+		private int LatitudeToYAtZoom(double lat, int zoom)
+		{
+			double arc = earthCircum / ((1 << zoom) * 256);
+			double sinLat = Math.sin(DegToRad(lat));
+			double metersY = earthRadius / 2 * Math.log((1 + sinLat) / (1 - sinLat));
+			int y = (int)Math.round((earthHalfCirc - metersY) / arc);
+			return y;
+		}
+
+		private int LongitudeToXAtZoom(double lon, int zoom)
+		{
+			double arc = earthCircum / ((1 << zoom) * 256);
+			double metersX = earthRadius * DegToRad(lon);
+			int x = (int)Math.round((earthHalfCirc + metersX) / arc);
+			return x;
+		}
+
+		private int GetZoomLevelByTrueViewRange(double trueViewRange)
+		{
+			int maxLevel = 3;
+			int minLevel = 19;
+			int numLevels = minLevel - maxLevel + 1;
+			int retLevel = maxLevel;
+			for(int i=0; i<numLevels; i++)
+			{
+				retLevel = i + maxLevel;
+
+				double viewAngle = 180;
+				for(int j=0; j<i; j++)
+				{
+					viewAngle = viewAngle / 2.0;
+				}
+				if(trueViewRange >= viewAngle)
+				{
+					break;
+				}
+			}
+			return retLevel;
+		}
+
+    	
+		/**
+		 * @param mapType the mapType to set
+		 */
+		public void setMapType(String mapType) {
+			this.mapType = mapType;
+		}
+
+		/**
+		 * @return the mapType
+		 */
+		public String getMapType() {
+			return mapType;
+		}
+
+    }
+
+    @Override
+    public String toString()
+    {
+    	// load from props
+        return Messages.getText("layers.Earth.MSVE.Name");
+    }
+
+	/**
+	 * @param mapType the mapType to set
+	 */
+	public void setMapType(String mapType) {
+		// TODO: builder.setMapType(mapType);
+	}
+
+	/**
+	 * @return the mapType
+	 */
+	public String getMapType() {
+		return ""; // TODO: builder.getMapType();
+	}
+	
+}
