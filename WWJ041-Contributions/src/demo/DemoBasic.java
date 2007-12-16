2b50cdb1e897d3616ec8246e120860cca1fe13bc
vladimir.silva.ortiz <vladimir.silva.ortiz@6ca97c9e-103c-0410-a6d1-f1f325dd6ca3>
2007-12-16 20:28:33 +0000

    Initial import.

---
+++ WWJ041-Contributions/src/demo/DemoBasic.java
@@ -0,0 +1,246 @@
+/* Copyright (C) 2001, 2006 United States Government as represented by
+the Administrator of the National Aeronautics and Space Administration.
+All Rights Reserved.
+*/
+package demo;
+
+import gov.nasa.worldwind.*;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.awt.*;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.layers.*;
+import gov.nasa.worldwind.layers.Earth.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+import worldwind.contrib.layers.GroundOverlayLayer;
+import worldwind.contrib.layers.PositionLayer;
+import worldwind.contrib.layers.loop.TimeLoopGroundOverlay;
+import worldwind.contrib.layers.quadkey.GoogleEarthLayer;
+import worldwind.contrib.layers.quadkey.GoogleRoadsLayer;
+import worldwind.contrib.layers.quadkey.VirtualEarthLayer;
+import worldwind.contrib.parsers.ParserUtils;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+
+/**
+ * @author tag
+ * @version $Id: WWPieceMaker.java 1764 2007-05-07 20:01:57Z tgaskins $
+ */
+public class DemoBasic
+{
+
+    private static class AppFrame extends JFrame
+    {
+
+        private final WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+    	
+        private DemoBasic.LayerAction[] layers = new DemoBasic.LayerAction[] {
+            new DemoBasic.LayerAction(new BMNGSurfaceLayer(), true, wwd),
+            new DemoBasic.LayerAction(new LandsatI3(), false, wwd),
+            new DemoBasic.LayerAction(new USGSDigitalOrtho(), false, wwd),
+            new DemoBasic.LayerAction(new USGSUrbanAreaOrtho(), false, wwd),
+            new DemoBasic.LayerAction(new EarthNASAPlaceNameLayer(), true, wwd),
+            new DemoBasic.LayerAction(new CompassLayer(), false, wwd),
+            new DemoBasic.LayerAction(new WorldMapLayer(), false, wwd),
+            new DemoBasic.LayerAction(new VirtualEarthLayer(), false, wwd),
+            new DemoBasic.LayerAction(new GoogleEarthLayer(), false, wwd),
+            new DemoBasic.LayerAction(new GoogleRoadsLayer(), false, wwd),
+            new DemoBasic.LayerAction(new PositionLayer(wwd), false, wwd),
+            null
+        };
+    	
+        public AppFrame() //DemoBasic.LayerAction[] layers)
+        {
+            LayerList layerList = new LayerList();
+
+            Model m = (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
+            m.setShowWireframeExterior(false);
+            m.setShowWireframeInterior(false);
+            m.setShowTessellationBoundingVolumes(false);
+            m.setLayers(layerList);
+            wwd.setModel(m);
+            
+            try
+            {
+                JPanel mainPanel = new JPanel();
+                mainPanel.setLayout(new BorderLayout());
+                wwd.setPreferredSize(new Dimension(800, 600));
+                mainPanel.add(wwd, BorderLayout.CENTER);
+
+                StatusBar statusBar = new StatusBar();
+                statusBar.setEventSource(wwd);
+                mainPanel.add(statusBar, BorderLayout.PAGE_END);
+                this.getContentPane().add(mainPanel, BorderLayout.CENTER);
+            
+                TimeLoopGroundOverlay anim = buildAnimatedGroundOverlay(wwd);
+                layers[layers.length - 1] = new DemoBasic.LayerAction(anim, false, wwd);
+            	
+                JPanel westContainer = new JPanel(new BorderLayout());
+                {
+                    JPanel westPanel = new JPanel(new GridLayout(0, 1, 0, 10));
+                    westPanel.setBorder(BorderFactory.createEmptyBorder(9, 9, 9, 9));
+                    {
+                        JPanel layersPanel = new JPanel(new GridLayout(0, 1, 0, 15));
+                        layersPanel.setBorder(new TitledBorder("Layers"));
+                        
+                        for (DemoBasic.LayerAction action : layers)
+                        {
+                            JCheckBox jcb = new JCheckBox(action);
+                            jcb.setSelected(action.selected);
+                            layersPanel.add(jcb);
+                            layerList.add(action.layer);
+
+                            if (action.layer instanceof TiledImageLayer)
+                                ((TiledImageLayer) action.layer).setShowImageTileOutlines(false);
+
+                            if (action.layer instanceof LandsatI3)
+                                ((TiledImageLayer) action.layer).setDrawBoundingVolumes(false);
+
+                            if (action.layer instanceof USGSDigitalOrtho)
+                                ((TiledImageLayer) action.layer).setDrawTileIDs(false);
+                        }
+                        
+                        
+                        westPanel.add(layersPanel);
+                        westContainer.add(westPanel, BorderLayout.NORTH);
+                    }
+                }
+
+                this.getContentPane().add(westContainer, BorderLayout.WEST);
+                this.pack();
+
+                Dimension prefSize = this.getPreferredSize();
+                prefSize.setSize(prefSize.getWidth(), 1.1 * prefSize.getHeight());
+                this.setSize(prefSize);
+
+                // Center the app on the user's screen.
+                Dimension parentSize;
+                Point parentLocation = new Point(0, 0);
+                parentSize = Toolkit.getDefaultToolkit().getScreenSize();
+                int x = parentLocation.x + (parentSize.width - prefSize.width) / 2;
+                int y = parentLocation.y + (parentSize.height - prefSize.height) / 2;
+                this.setLocation(x, y);
+                this.setResizable(true);
+
+                
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+     * Animated screen overlay example: Toxic clod of mount pinatubo eruption
+     */
+    static private TimeLoopGroundOverlay buildAnimatedGroundOverlay(WorldWindowGLCanvas canvas) 
+    {
+        TimeLoopGroundOverlay overlay = 
+        	new TimeLoopGroundOverlay("Pinatubo Toxic Cloud", canvas); 
+        
+    	Sector bbox = new Sector(Angle.fromDegrees(-18.75), Angle.fromDegrees(28.75),
+                Angle.fromDegrees(-65), Angle.fromDegrees(125));
+
+    	String  pref = "http://svs.gsfc.nasa.gov/cgi-bin/wms?"
+    		+ "service=WMS&request=GetMap&layers=3169_21478&bbox=-65,-18.75,125,28.75"
+    		+ "&width=1024&height=256&srs=EPSG:4326&format=image/png&version=1.1.1";
+    	
+    	// Pinatubo cloud time span: 1991-06-16/1991-06-29/P1D
+    	try {
+    		final String datePref = "1991-06-";
+    		
+    		for (int i = 16; i <= 29; i++) 
+    		{
+    			final String icon = pref + "&time=" + datePref + i;
+
+            	overlay.add(new GroundOverlayLayer(datePref + i
+            			, bbox
+            			, new URL(icon)
+            			, ParserUtils.getIconSuffix(icon)
+            	));
+			}
+		} 
+    	catch (Exception e) {
+			e.printStackTrace();
+		}
+        return overlay;
+    }
+
+    private static class LayerAction extends AbstractAction
+    {
+        private Layer layer;
+        private boolean selected;
+        private WorldWindowGLCanvas canvas;
+        
+        public LayerAction(Layer layer, boolean selected, WorldWindowGLCanvas canvas)
+        {
+            super(layer.getName());
+            this.layer = layer;
+            this.selected = selected; 
+            this.layer.setEnabled(this.selected);
+            this.canvas = canvas;
+        }
+
+        public void actionPerformed(ActionEvent actionEvent)
+        {
+        	
+            if (((JCheckBox) actionEvent.getSource()).isSelected()) 
+            {
+                this.layer.setEnabled(true);
+                if (layer instanceof TimeLoopGroundOverlay) 
+                {
+                	System.out.println("play");
+                	((TimeLoopGroundOverlay)layer).addToModel(canvas);
+	    			((TimeLoopGroundOverlay)layer).play();
+                }
+            }
+            else {
+                this.layer.setEnabled(false);
+                if (layer instanceof TimeLoopGroundOverlay) {
+	    			((TimeLoopGroundOverlay)layer).stop();
+	    			//((TimeLoopGroundOverlay)layer).removeFromModel(canvas.getModel());
+              }
+            }
+
+            appFrame.wwd.repaint();
+        }
+    }
+
+    static
+    {
+        if (Configuration.isMacOS())
+        {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "World Wind Basic Demo");
+            System.setProperty("com.apple.mrj.application.growbox.intrudes", "false");
+        }
+    }
+
+    private static DemoBasic.AppFrame appFrame;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Java run-time version: " + System.getProperty("java.version"));
+        System.out.println(gov.nasa.worldwind.Version.getVersion());
+
+        try
+        {
+            DemoBasic demo = new DemoBasic();
+            appFrame = new DemoBasic.AppFrame(); //demo.layers);
+            appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            appFrame.setVisible(true);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ WWJ041-Contributions/src/demo/GlobeSelectionListener.java
@@ -0,0 +1,141 @@
+package demo;
+
+import worldwind.contrib.layers.PlacemarkLayer;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.event.SelectEvent;
+import gov.nasa.worldwind.event.SelectListener;
+import gov.nasa.worldwind.examples.BasicDragger;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.globes.Globe;
+import gov.nasa.worldwind.layers.Earth.WorldMapLayer;
+import gov.nasa.worldwind.pick.PickedObject;
+import gov.nasa.worldwind.pick.PickedObjectList;
+import gov.nasa.worldwind.render.Annotation;
+import gov.nasa.worldwind.render.WWIcon;
+import gov.nasa.worldwind.view.FlyToOrbitViewStateIterator;
+import gov.nasa.worldwind.view.OrbitView;
+
+/**
+ * Selection listener
+ * @author Owner
+ *
+ */
+public class GlobeSelectionListener implements SelectListener 
+{
+    private WWIcon lastToolTipIcon = null;
+    private BasicDragger dragger = null;
+    private WorldWindowGLCanvas world;
+    private WWIcon lastPickedIcon;
+	
+	public GlobeSelectionListener(WorldWindowGLCanvas canvas) {
+		dragger = new BasicDragger(canvas);
+		world = canvas;
+	}
+	
+
+    private void highlight(Object o)
+    {
+        if (this.lastPickedIcon == o)
+            return; // same thing selected
+
+        if (this.lastPickedIcon != null)
+        {
+            this.lastPickedIcon.setHighlighted(false);
+            this.lastPickedIcon = null;
+        }
+
+        if (o != null && o instanceof WWIcon)
+        {
+            this.lastPickedIcon = (WWIcon) o;
+            this.lastPickedIcon.setHighlighted(true);
+        }
+    }
+
+    /**
+     * Select
+     */
+    public void selected(SelectEvent event)
+    {
+        if (event.getEventAction().equals(SelectEvent.LEFT_CLICK))
+        {
+            if (event.hasObjects())
+            {
+                System.out.println("Single clicked " + event.getTopObject()
+                		+ " of class:" + event.getTopObject().getClass().getName());
+                
+                // Annotation clicked
+                if (event.getTopObject() instanceof Annotation)
+                {
+                    // Check for text or url
+                    PickedObject po = event.getTopPickedObject();
+                    
+                    if(po.getValue(AVKey.TEXT) != null)
+                    {
+                        System.out.println("Text: \"" + po.getValue(AVKey.TEXT) + "\" Hyperlink: "  + po.getValue(AVKey.URL));
+                    }
+                }
+                else if (event.getTopObject() instanceof WorldMapLayer)
+                {
+                    // Left click on World Map : iterate view to target position
+                    Position targetPos = event.getTopPickedObject().getPosition();
+                    OrbitView view = (OrbitView)world.getView();
+                    Globe globe = world.getModel().getGlobe();
+                    // Use a PanToIterator
+                    view.applyStateIterator(FlyToOrbitViewStateIterator.createPanToIterator(
+                            view, globe, new LatLon(targetPos.getLatitude(), targetPos.getLongitude()),
+                            Angle.ZERO, Angle.ZERO, targetPos.getElevation()));
+                }
+                else if  (event.getTopObject() instanceof WWIcon ) 
+                {
+                	// toggle icon annotation
+                	PlacemarkLayer.PlacemarkIcon icon = (PlacemarkLayer.PlacemarkIcon)event.getTopObject();
+                	
+                	icon.getAnnotation().getAttributes().setVisible(
+                			! icon.getAnnotation().getAttributes().isVisible());
+                	
+                }
+            }
+            else
+                System.out.println("Single clicked " + "no object");
+        }
+        else if (event.getEventAction().equals(SelectEvent.HOVER))
+        {
+            if (lastToolTipIcon != null)
+            {
+                lastToolTipIcon.setShowToolTip(false);
+                this.lastToolTipIcon = null;
+                world.repaint();
+            }
+
+            if (event.hasObjects() && !this.dragger.isDragging())
+            {
+                if (event.getTopObject() instanceof WWIcon)
+                {
+                    this.lastToolTipIcon = (WWIcon) event.getTopObject();
+                    lastToolTipIcon.setShowToolTip(true);
+                    world.repaint();
+                }
+            }
+        }
+        else if (event.getEventAction().equals(SelectEvent.ROLLOVER) && !this.dragger.isDragging())
+        {
+            highlight(event.getTopObject());
+        }
+        else if (event.getEventAction().equals(SelectEvent.DRAG_END)
+            || event.getEventAction().equals(SelectEvent.DRAG))
+        {
+            // Delegate dragging computations to a dragger.
+            this.dragger.selected(event);
+            if (event.getEventAction().equals(SelectEvent.DRAG_END))
+            {
+                PickedObjectList pol = world.getObjectsAtCurrentPosition();
+                if (pol != null)
+                    highlight(pol.getTopObject());
+            }
+        }
+    }
+
+}
